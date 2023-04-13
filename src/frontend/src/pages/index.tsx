8d0c627a53b4caf28ae4c19462208f438208ab7c
Matheus Victor <matheusvictor@ufpi.edu.br>
2023-04-13 17:14:10 -0300

    Criação do projeto FrontEnd com NextJS

---
+++ src/frontend/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Sujeito Pizza</h1>
+    </div>
+  )
+}
