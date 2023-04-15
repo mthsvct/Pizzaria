21c547662c1fa74d36808bbc3e3ac4002a1025d4
Matheus Victor <matheusvictor@ufpi.edu.br>
2023-04-15 00:41:43 -0300

    Criação e estilização da página de cadastro

--- src/frontend/src/pages/signup/index.tsx
+++ src/frontend/src/pages/signup/index.tsx
@@ -2,11 +2,11 @@
 import Head from 'next/head';
 import Image from 'next/image';
 
-import logoImg from '../../public/logo.svg';
-import styles from '../../styles/home.module.scss';
+import logoImg from '../../../public/logo.svg';
+import styles from '../../../styles/home.module.scss';
 
-import { Input, TextArea } from '../components/ui/input';
-import { Button } from '../components/ui/button';
+import { Input, TextArea } from '@/src/components/ui/input';
+import { Button } from '@/src/components/ui/button';
 
 import Link from 'next/link';
 
@@ -19,20 +19,22 @@ export default function Home() {
 	return (
 		<>
 			<Head>
-				<title>Sujeito Pizza - Faça seu login;</title>
+				<title>Faça seu cadastro!</title>
 			</Head>
 
 			<div className={styles.containerCenter}>
 				<Image src={logoImg} alt='Logo Sujeito Pizzaria' />
 				
 				<div className={styles.login}>
+                    <h1>Criando sua conta</h1>
 					<form>
+                        <Input placeholder='Digite seu nome: ' type='text' />
 						<Input placeholder='Digite seu email: ' type='email' />
 						<Input placeholder='Digite sua senha: ' type='password' />
 						<Button type="submit" loading={false}>Fazer Login</Button>
 					</form>
-					<Link href="/signup" legacyBehavior>
-						<a className={styles.text}>Não possui uma conta? Cadastre-se</a>
+					<Link href="/" legacyBehavior>
+						<a className={styles.text}>Já possui uma conta? Faça login!</a>
 					</Link>
 				</div>
 			</div>
